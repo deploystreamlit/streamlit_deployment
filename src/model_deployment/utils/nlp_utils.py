4bc6253d62850a2b8bfa8a85de552cda59edafff
adamsebastiangorski <adamsebastiangorski@gmail.com>
2023-08-08 11:27:50 +0200

    asd

--- src/model_deployment/utils/nlp_utils.py
+++ src/model_deployment/utils/nlp_utils.py
@@ -70,7 +70,8 @@ def calculate_output_classes(ort_outputs: List[NDArray]) -> List[int]:
 
 def get_ort_session() -> InferenceSession:
     model_path = os.getenv("MODEL_PATH")
-    return onnxruntime.InferenceSession(model_path)
+    return None
+    # return onnxruntime.InferenceSession(model_path)
 
 
 def run_inference(session: InferenceSession, ort_inputs: Dict[str, NDArray]) -> Tuple[
